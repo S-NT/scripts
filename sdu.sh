51572c870bc9f6205d55d89c973702df23cc6986
S-NT <hidden@email>
2021-06-15 04:09:41 +0300

    [sdu.sh] Add possibility to change a size unit (KB, MB, etc)

--- sdu.sh
+++ sdu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Sorted du (disk usage) output, v.0.1.1
+# Sorted du (disk usage) output, v.1.0.3
 #
 #The MIT License (MIT)
-#Copyright (c) 2015 S-NT  (https://github.com/S-NT/scripts)
+#Copyright (c) 2021 S-NT  (https://github.com/S-NT/scripts)
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -22,27 +22,72 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-script_name=`echo $0 | sed "s/^.\+\///gi"`
+script_name=`basename $0`
 du_sys=/usr/bin/du
 sort_sys=/usr/bin/sort
 
-if [ -z "$1" ]
-  then
-    echo -e "SYNTAX:\n\t${script_name} /path/to/directory"
-    exit 1
-fi
-
 # Checking for alternative path to 'sort' (CentOS related)
-if [ ! -x $sort_sys ]
+if [ ! -x "$sort_sys" ]
   then
     sort_sys=/bin/sort
-    if [ ! -x $sort_sys ]
+    if [ ! -x "$sort_sys" ]
       then
         echo "(x) Cannot find 'sort' executable!"
         exit 2
     fi
 fi
 
-$du_sys -am --max-depth=1 "$1" | $sort_sys -n
+
+show_help() {
+  echo -e "SYNTAX:\n\t${script_name} [-b|-k|-m|-g] <path_to_directory>"
+  echo -e "\t-b\tscale sizes by Bytes"
+  echo -e "\t-k\tscale sizes by KB"
+  echo -e "\t-m\tscale sizes by MB, default option"
+  echo -e "\t-g\tscale sizes by GB"
+}
+
+
+while getopts bkmg opt
+  do
+    case "$opt" in
+      b)
+        block_size="1"
+        units="bytes"
+      ;;
+      k)
+        block_size="1K"
+        units="KB"
+      ;;
+      m)
+        block_size="1M"
+        units="MB"
+      ;;
+      g)
+        block_size="1G"
+        units="GB"
+      ;;
+      *)
+        show_help
+        exit 1
+      ;;
+    esac
+  done
+
+if [ -z "$block_size" ]
+  then
+    block_size="1M"
+    units="MB"
+fi
+
+shift $(($OPTIND - 1)) && path=$1
+
+if [ -z "$path" ]
+  then
+    show_help
+    exit 1
+fi
+
+echo " [ ${units} ]"
+${du_sys} -a --block-size="${block_size}" --max-depth=1 "${path}" | ${sort_sys} -n
 
 exit 0
